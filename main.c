8f4f02fa864f81f7f5243db5cfc963e0724f59ad
paulbgtr <pbg.work@pm.me>
2026-02-13 17:19:22 +0200

    implement function to get new task id

--- main.c
+++ main.c
@@ -13,6 +13,28 @@ void print_task(Task task)
     printf("id: %d | title: %s | completed: %d\n", task.id, task.title, task.completed);
 }
 
+int get_new_id(FILE *fptr)
+{
+    fptr = fopen("tasks.txt", "r");
+
+    char buffer[256];
+
+    int id;
+
+    while (fgets(buffer, sizeof(buffer), fptr) != NULL)
+    {
+        sscanf(buffer, "%d", &id);
+    }
+    fclose(fptr);
+
+    if (id == 0)
+    {
+        return 0;
+    }
+
+    return id + 1;
+}
+
 int main()
 {
     char command;
@@ -30,13 +52,15 @@ int main()
 
         fptr = fopen("tasks.txt", "a");
 
-        if (fptr == NULL)
+        if (!fptr)
         {
             perror("adding new task:");
             return 1;
         }
 
-        fprintf(fptr, "0 %99s 0\n", title);
+        int new_id = get_new_id(fptr);
+
+        fprintf(fptr, "%d %s 0\n", new_id, title);
 
         fclose(fptr);
         return 0;
