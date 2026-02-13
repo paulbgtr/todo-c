586fb18743a8456df64313e2b7db11d6b4387e3e
paulbgtr <pbg.work@pm.me>
2026-02-13 16:59:06 +0200

    implement writing to file

--- main.c
+++ main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    int id;
+    char title[100];
+    int completed; // 0 / 1
+} Task;
+
+void print_task(Task task)
+{
+    printf("id: %d | title: %s | completed: %d\n", task.id, task.title, task.completed);
+}
 
 int main()
 {
-    printf("hello");
+    char command;
+
+    FILE *fptr;
+
+    scanf(" %c", &command);
+
+    if (command == 'n')
+    {
+        char title[100];
+
+        printf("enter task title: ");
+        scanf("%99s", title);
+
+        fptr = fopen("tasks.txt", "a");
+
+        if (fptr == NULL)
+        {
+            perror("adding new task:");
+            return 1;
+        }
+
+        fprintf(fptr, "0 %99s 0\n", title);
+
+        fclose(fptr);
+        return 0;
+    }
+
+    printf("unsupported command!\n");
 
     return 0;
 }
